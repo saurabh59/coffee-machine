0bc084eaa6d76a956b9a015a87a31d2b09632c3a
saurabh59 <30611169+saurabh59@users.noreply.github.com>
2020-05-06 19:08:43 +0530

    created_stage2

---
+++ stage2.py
@@ -0,0 +1,9 @@
+print('Write how many cups of coffee you will need:')
+n = int(input())
+wr = 200 * n
+mr = 50 * n
+cb = 15 * n 
+print("For ", n, " cups of coffee you will need:")
+print(wr, " ml of water")
+print(mr, " ml of milk")
+print(cb, " g of coffee beans")
